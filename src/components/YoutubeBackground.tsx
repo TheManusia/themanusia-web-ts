c9474b3cab768a147f781a218dae37f7d084bdc2
Ian <minecraftindo0@gmail.com>
2023-03-18 18:45:06 +0700

    Change background if video paused

--- src/components/YoutubeBackground.tsx
+++ src/components/YoutubeBackground.tsx
@@ -1,8 +1,10 @@
 import YouTube from 'react-youtube';
+import {useState} from "react";
 
 function YoutubeBackground({onReady}: { onReady: Function }) {
     const videoIds = ['BnkhBwzBqlQ', "_YnwnxSE2UA", "fbJiRfIp_cU"];
     const videoId = videoIds[0];
+    const [paused, setPaused] = useState(false);
     let videoNumber = 0;
 
     const opts = {
@@ -28,11 +30,22 @@ function YoutubeBackground({onReady}: { onReady: Function }) {
         event.target.loadVideoById(videoIds[videoNumber]);
     }
 
+    const onStateChanged = (event: any) => {
+        if (event.data === 1) {
+            setPaused(false);
+        } else {
+            setPaused(true);
+        }
+    }
+
     return (
         <div className="absolute top-0 left-0 w-full h-full z-0">
             <YouTube videoId={videoId} opts={opts}
                      className="absolute top-0 left-0 w-full h-screen z-0 aspect-[4/3]"
-                     onEnd={onEnd} onReady={(event) => onReady(event)}/>
+                     onEnd={onEnd} onReady={(event) => onReady(event)}
+                     onStateChange={(event) => onStateChanged(event)}/>
+            <div
+                className={`absolute top-0 left-0 w-full h-screen z-0 bg-black ${paused ? "opacity-100" : "opacity-0"}`}/>
             <div className="absolute top-0 left-0 w-full h-full z-0 bg-black opacity-75"/>
         </div>
     )
